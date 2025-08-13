49905cde0d9e39a94e245e68bba86d3329c90148
israchiekh2-cmyk <israchiekh2@gmail.com>
2025-08-13 14:14:18 +0200

    Update App.js

--- components/App.js
+++ components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Composants principaux
@@ -76,6 +76,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
